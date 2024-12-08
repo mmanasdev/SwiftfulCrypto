505d48137ccb34d759a91f2799fd051c9acddbf2
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-12-08 12:05:23 +0100

    Revert "Filtering data based on search bar text using Combine | SwiftUI Crypto App #11"
    This reverts commit 7e474098ee063901374c7049f08e57365470c78c.

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,12 +10,6 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
-    ]
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
@@ -28,26 +22,10 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        
-        $searchText
-            .combineLatest(dataService.$allCoins)
-            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+        dataService.$allCoins
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
-    }
-    
-    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
-        guard !text.isEmpty else {
-            return coins
-        }
-        let lowercasedText = text.lowercased()
-        return coins
-            .filter {   $0.name.lowercased().contains(lowercasedText) ||
-                        $0.symbol.lowercased().contains(lowercasedText) ||
-                        $0.id.lowercased().contains(lowercasedText)
-            }
     }
 }
