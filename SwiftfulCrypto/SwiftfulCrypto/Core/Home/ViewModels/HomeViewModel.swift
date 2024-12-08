b68caabfaf0d213faab007b869cca7e9486c7b4b
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-12-08 12:17:55 +0100

    Filtering data based on search bar text using Combine | SwiftUI Crypto App #11

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -28,10 +28,26 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowercasedText = text.lowercased()
+        return coins
+            .filter {   $0.name.lowercased().contains(lowercasedText) ||
+                $0.symbol.lowercased().contains(lowercasedText) ||
+                $0.id.lowercased().contains(lowercasedText)
+            }
     }
 }
